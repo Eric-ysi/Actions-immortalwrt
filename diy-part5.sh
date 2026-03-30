a55fb12849a8719c0e5fc9b59b0172ecd52125a6
xuxin1955 <73999603+xuxin1955@users.noreply.github.com>
2026-03-31 00:23:35 +0800

    Update diy-part5.sh

--- diy-part5.sh
+++ diy-part5.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # packages
-sed -i 's|src-git packages https://github.com/[^/]*/packages|src-git packages https://github.com/xuxin1955/packages|' feeds.conf.default
+# sed -i 's|src-git packages https://github.com/[^/]*/packages|src-git packages https://github.com/xuxin1955/packages|' feeds.conf.default
 
 # password
-sed -i '/sed -i .*root::0:0:.*$1\$V4UetPzk\$CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
-sed -i '/sed -i .*root:::0:.*$1\$V4UetPzk\$CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
+# sed -i '/sed -i .*root::0:0:.*$1\$V4UetPzk\$CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
+# sed -i '/sed -i .*root:::0:.*$1\$V4UetPzk\$CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
 
 
 
